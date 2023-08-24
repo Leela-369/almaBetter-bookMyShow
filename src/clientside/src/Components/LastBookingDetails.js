dfdcbeff65b27969257b56afa7d78771be8c2329
Leela-369 <gonnabathulaleelasanthosh@gmail.com>
2023-08-25 01:57:15 +0530

    add loaders

--- src/clientside/src/Components/LastBookingDetails.js
+++ src/clientside/src/Components/LastBookingDetails.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, CircularProgress } from '@mui/material';
 import axios from 'axios';
 
 export const LastBookingDetails = () => {
   const [lastBooking, setLastBooking] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false); // Add state to track error
 
   useEffect(() => {
+    setIsLoading(true);
+
     const BASE_URL= process.env.REACT_APP_BASE_URL
     // Make the GET request to fetch the last booking details
     axios.get(`${BASE_URL}/api/last`)
@@ -18,13 +21,18 @@ export const LastBookingDetails = () => {
       .catch(error => {
         console.error('Error fetching last booking:', error);
         setError(true); // Set error state to true on error
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [lastBooking]);
 
   return (
     <div>
       <Box sx={{ width: ['auto','auto','12rem'], minHeight: '12rem', border: 2, padding: 3,display:'flex', justifyContent:'center', alignItems:'center', }}>
-        {error ? ( // Display error message conditionally
+      {isLoading ? (
+          <CircularProgress sx={{color: '#33eaff'}} /> // Display loading indicator
+        ) : error ? ( // Display error message conditionally
           <Typography>No Bookings Yet</Typography>
         ) : (
           <div>
